49c777dfbf4608a628c87f7e978d81eda8977533
zhangxk <zhangxk25@163.com>
2020-08-03 20:15:41 +0800

    .

---
+++ 各种排序/index.js
@@ -0,0 +1,26 @@
+const arr = [1, 6, 2, 8, 3, 5, 8, 2, 9, 3, 1, 8, 0, 3, 7, 0];
+
+/* 冒泡排序 */
+function bubble(arr) {
+  const handleArr = arr.slice();
+  let isSort = true; // 记录是否已经排好了
+  for (let i = 0; i < handleArr.length - 1; i++) {
+    for (let j = 0; j < handleArr.length - 1 - i; j++) {
+      if (handleArr[j] > handleArr[j + 1]) {
+        /* 引入第三变量的常规方式值交换 */
+        // const temp = handleArr[j];
+        // handleArr[j] = handleArr[j + 1];
+        // handleArr[j + 1] = temp;
+        /* 使用es6解构的值交换 */
+        [handleArr[j], handleArr[j + 1]] = [handleArr[j + 1], handleArr[j]];
+        isSort = false;
+      }
+    }
+    if (isSort) {
+      break;
+    }
+  }
+  return handleArr;
+}
+
+console.log(bubble(arr), '---18--mark2020');
